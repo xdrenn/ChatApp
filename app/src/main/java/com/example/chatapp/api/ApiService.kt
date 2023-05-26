c2fd53dd12842a0bdf1985adef137aeca1d4e44b
Drenn <burberrry3@gmail.com>
2023-05-26 18:58:39 +0300

    Added new post request

--- app/src/main/java/com/example/chatapp/api/ApiService.kt
+++ app/src/main/java/com/example/chatapp/api/ApiService.kt
@@ -1,10 +1,12 @@
 package com.example.chatapp.api
 
-import com.example.chatapp.api.request.Auth
-import com.example.chatapp.api.request.Code
+import com.example.chatapp.api.request.AuthRequest
+import com.example.chatapp.api.request.CodeRequest
+import com.example.chatapp.api.request.RegisterRequest
 import com.example.chatapp.api.response.AuthResponse
 import com.example.chatapp.api.response.CodeResponse
 import com.example.chatapp.api.response.RefreshTokenResponse
+import com.example.chatapp.api.response.RegisterResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.Header
@@ -15,14 +17,22 @@ interface ApiService {
     @Headers("Content-Type: application/json")
     @POST("api/v1/users/send-auth-code/")
     suspend fun auth(
-        @Body auth: Auth
+        @Body authRequest: AuthRequest
     ): Response<AuthResponse>
 
     @Headers("Content-Type: application/json")
     @POST("api/v1/users/check-auth-code/")
     suspend fun code(
-        @Body code: Code
+        @Body codeRequest: CodeRequest
     ): Response<CodeResponse>
+
+    @Headers("Content-Type: application/json")
+    @POST("/api/v1/users/register/")
+    suspend fun registration(
+        @Body registerRequest: RegisterRequest
+    ): Response<RegisterResponse>
+
+
 
     @Headers("Content-Type: application/json")
     @POST("api/v1/users/refresh-token/")
